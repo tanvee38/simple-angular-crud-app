79bee0e40d6bf835319a5bec5bda6d1d89d54b84
Moin <moin.mahmud38@gmail.com>
2025-03-22 09:01:00 -0600

    Update crud.service.ts

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -18,6 +18,7 @@ export class CrudService {
     // use this backend url when you use and run restApi .net core api service. 
     // This RestApi project is in my github repo too.
     // this.serviceURL = "http://localhost:5001/api/Todos";
+    // update with todo api in azure
     this.serviceURL = "https://todo-api-app-bve7g4h4b4c4fagt.canadacentral-01.azurewebsites.net/api/todos";
   }
 
